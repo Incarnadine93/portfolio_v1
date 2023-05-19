ce3d961f9e5e567e0566d9e24e9b8f6aa80ce855
unknown <anpham93@hotmail.com>
2023-05-19 08:09:37 -0700

    added footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./*html"],
   theme: {
     extend: {
       screens:{
-        "sm": "480px",
+        sm:"480px",
+        md:"768px",
+        lg:"1024px",
+        xl:"1280px",
       },
       spacing:{
         "big":"48rem"
